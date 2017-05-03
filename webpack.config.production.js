4d8a7ebb4cbe3d78e2233a6dae64eafd74b39a4f
Sam Lau <samlau95@gmail.com>
2017-05-03 00:16:48 -0700

    Deploy

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -18,18 +18,22 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.jsx?$/,
+        use: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/,
+      },
+      {
         test: /\.json$/,
         use: 'json-loader',
         exclude: /node_modules/,
       },
       {
-        test: /\.jsx?$/,
-        use: [
-          'babel-loader'
-        ],
-        exclude: /node_modules/
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       }
-    ]
+    ],
   },
 
   plugins: [
