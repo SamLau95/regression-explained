c416544eba5d2551c0b8c0044d7b11d83b10c55f
Sam Lau <samlau95@gmail.com>
2017-05-05 23:35:51 -0700

    Add conclusion

--- src/App.js
+++ src/App.js
@@ -21,22 +21,66 @@ if (module.hot) {
 
 export default class App extends Component {
   render() {
-    return <Provider store={store}>
-      <main>
-        <div className="masthead">
-          <h1 className="masthead__title">Regression, Explained Visually</h1>
-          <div className="masthead__author">
-            <a href="http://www.samlau.me/" rel="author" target="_blank">
-              by Sam Lau
-            </a>
+    return (
+      <Provider store={store}>
+        <main>
+          <div className="masthead">
+            <h1 className="masthead__title">Regression, Explained Visually</h1>
+            <div className="masthead__author">
+              <a href="http://www.samlau.me/" rel="author" target="_blank">
+                by Sam Lau
+              </a>
+            </div>
           </div>
-        </div>
 
-        <Linear />
-        <Polynomial />
-        <TrainingError />
-        <Validation />
-      </main>
-    </Provider>;
+          <Linear />
+          <Polynomial />
+          <TrainingError />
+          <Validation />
+
+          <div>
+            <h2>Conclusion</h2>
+
+            <p>
+              We've just gone through quite a bit of material. We started out
+              with what regression is and why it's useful. We then discussed
+              linear regression and the least squares cost. Polynomial regression
+              came next, and we explored the behavior of varying the degree of
+              the polynomial when fitted to the same data. We concluded with a
+              discussion of training vs. validation error and why we use
+              cross-validation to select our model.
+            </p>
+
+            <p>
+              I hope this article was helpful to you! Feel free to check out the
+              code <a href="https://github.com/SamLau95/regression-explained">
+                on Github
+              </a>
+              {' '}
+              and
+              {' '}
+              <a href="https://github.com/SamLau95/regression-explained/issues/new">
+                leave an issue
+              </a>
+              {' '}
+              with any comments or suggestions.
+            </p>
+
+            <p>
+              This web app was created for the final project of
+              {' '}
+              <a href="https://people.eecs.berkeley.edu/~jrs/189/">
+                UC Berkeley's Machine Learning class, CS289A
+              </a>
+              . The dataset for this article was taken from
+              {' '}
+              <a href="http://www.stat.ufl.edu/~winner/datasets.html">
+                the great collection of David Winner
+              </a>.
+            </p>
+          </div>
+        </main>
+      </Provider>
+    );
   }
 }
